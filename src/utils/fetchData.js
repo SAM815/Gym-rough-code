1df47076ae733fd0c01f378bb09957e65a6889d0
SAM815 <99433555+SAM815@users.noreply.github.com>
2023-03-25 16:13:11 -0400

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -12,7 +12,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': '2de48456e9mshf0da098ceb1dba0p16df21jsn69261a51476b',
+    'X-RapidAPI-Key': ,
    
   }
 };
